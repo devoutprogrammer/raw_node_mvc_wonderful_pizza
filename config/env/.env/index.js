5492a35cd7f7c3dcf19f746060925899d9395903
Ericson Weah <ericson.weah@utah.edu>
2021-06-04 11:23:30 -0600

    Here we go

---
+++ config/env/.env/index.js
@@ -0,0 +1,29 @@
+// Api keys and and token
+module.exports = {
+
+    // Stripe API Key
+    STRIPE: {
+        _API_KEY: 'YOUR_STRIP_API_KEY'
+    },
+
+    // Mailgun  API Key
+    MAILGUN: {
+        _API_KEY: 'YOUR_MAILGUN_API_KEY',
+        _DOMAIN: 'YOUR_MAILGUN_DOMAIN'
+     
+    },
+
+    // Twilio API Key
+    TWILIO: {
+        _ACCOUNT_SID: 'YOUR_TWILIO_ACCOUNT_SIDE',
+        _AUTH_TOKEN: 'YOUR_TWILIO_AUTH_TOKEN'
+
+    },
+    PASSWORD: {
+        _HASH: {
+            _SECRET: 'YOUR_SECRET_PHRASE',
+            _METHOD: 'HASHING_METHOD' // sha256, etc....
+        }
+    }
+}
+
